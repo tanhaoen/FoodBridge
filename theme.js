0afb14fbcce424d246bf22392883702725741262
tanhaoen <nathanieltan1701@gmail.com>
2024-03-07 22:22:33 +0800

    Created theme file to standardise color scheme

---
+++ theme.js
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'react-native-paper';
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: "#2DCC70",
+      secondary: "#00692C",
+      tertiary: "#3A656E",
+      background: "#ffffff",
+      surface: "#2DCC70",
+    }
+};
+
+export default theme;
