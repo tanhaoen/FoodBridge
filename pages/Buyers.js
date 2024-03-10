7dea42c1481319a445720dc9ebad048a5fe17f85
tanhaoen <nathanieltan1701@gmail.com>
2024-03-11 02:36:46 +0800

    Added Buyer page and card component

--- pages/Buyers.js
+++ pages/Buyers.js
@@ -1,10 +1,33 @@
-import { Text } from "react-native-paper";
+import { ScrollView, View } from "react-native";
+import { Button, Text } from "react-native-paper";
+import BuyerCard from "../components/BuyerCard";
 
 export default function Buyers() {
   
-    return (
-        <>
-        <Text>This is the Buyers page</Text>
-        </>
-    );
-  }
+	const sampleOrder = [
+		{
+			item: "Curry Chicken",
+			quantity: 3
+		}
+	]
+
+	return (
+		<View style={{ marginHorizontal: 16, marginTop: 30 }}>
+			<Button
+				icon="qrcode"
+				mode="contained"
+				onPress={() => console.log('Pressed')}
+			>
+				Scan buyer's QR code
+			</Button>
+			<ScrollView vertical>
+				<BuyerCard
+					buyerName="John Doe"
+					eta={5}
+					distance="500"
+					order={sampleOrder}
+				/>
+			</ScrollView>
+		</View>
+	);
+}
